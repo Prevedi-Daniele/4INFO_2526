c34054a3382398fbe044a9c50644cc2328e0a4f3
Prevedi-Daniele <danieleprevedi@gmail.com>
2025-11-06 15:30:58 +0100

    (Prevedi) Consegna Esercitazione 09 JS

--- TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_09.js
+++ TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_09.js
@@ -1,11 +1,7 @@
 function password(){
     let password = prompt("Immettere la password")
-    password == null && alert("Operazione annullata.")
-    password != "Agnell1no" && password != null && alert("Password errata.")
-    password == "Agnell1no" && alert("Benvenuto")
+    password == null && alert("Operazione annullata.") || password != "Agnell1no" && password != null && alert("Password errata.") || password == "Agnell1no" && alert("Benvenuto")
 }
 
 let username = prompt("Immettere l'username")
-username == null && alert("Operazione annullata.")
-username != "Admin" && username != null && alert("Non so chi tu sia.")
-username == "Admin" && password()
+username == null && alert("Operazione annullata.") || username != "Admin" && username != null && alert("Non so chi tu sia.") || username == "Admin" && password()
