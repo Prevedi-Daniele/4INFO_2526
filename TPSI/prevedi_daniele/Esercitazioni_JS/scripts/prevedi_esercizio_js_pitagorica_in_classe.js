e00383b4b50f78e04574479c5995df22890c5197
Prevedi-Daniele <danieleprevedi@gmail.com>
2025-11-20 13:35:07 +0100

    c2

--- TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_pitagorica_in_classe.js
+++ TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_pitagorica_in_classe.js
@@ -1,3 +1,11 @@
 function generaPitagorica(){
+    let colonne = document.getElementById("colonne").value // Prende il primo oggetto nel DOM che ha quell'ID
     
+    if(colonne < 4 || colonne > 12){
+        alert("Inserisci un numero valido, tra 4 e 12.")
+        return
+    }
+    else{
+        console.log("Numero valido")
+    }
 }
