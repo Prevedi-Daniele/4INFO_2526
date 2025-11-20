b3508f2d6671c715df3b1fd0fcbd9ded3f03bfb6
Prevedi-Daniele <danieleprevedi@gmail.com>
2025-11-20 13:47:05 +0100

    c6

--- TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_pitagorica_in_classe.js
+++ TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_pitagorica_in_classe.js
@@ -1,15 +1,20 @@
 function generaPitagorica(){
-    let colonne = document.getElementById("colonne").value // Prende il primo oggetto nel DOM che ha quell'ID
-    
+    let colonne = document.getElementById("colonne").value  // Prende il primo oggetto nel DOM che ha quell'ID
+    let tabella = document.getElementById("pitagorica")     // Prendo la tabella già esistente
+
     if(colonne < 4 || colonne > 12){
         alert("Inserisci un numero valido, tra 4 e 12.")
         return
     }
 
     for(let r = 0; r <= colonne; r++){
+        let riga = document.createElement("tr")
+        tabella.append(riga)                                // Aggiungo la riga alla tabella
         for(let c = 0; c <= colonne; c++){
-            let cella = document.createElement("td") // Crea un nuovo tag HTML che passo per stringa
+            let cella = document.createElement("td")        // Crea un nuovo tag HTML che passo per stringa
             cella.innerText = `${r} - ${c}`
+            riga.append(cella)                              // Aggiungo la cella alla riga
+
         }
     }
 }
