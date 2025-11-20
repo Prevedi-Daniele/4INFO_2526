a43697b93ea4b68a64e243e40ebea10d63d10f92
Prevedi-Daniele <danieleprevedi@gmail.com>
2025-11-20 13:49:22 +0100

    c7

--- TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_pitagorica_in_classe.js
+++ TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_pitagorica_in_classe.js
@@ -12,7 +12,9 @@ function generaPitagorica(){
         tabella.append(riga)                                // Aggiungo la riga alla tabella
         for(let c = 0; c <= colonne; c++){
             let cella = document.createElement("td")        // Crea un nuovo tag HTML che passo per stringa
-            cella.innerText = `${r} - ${c}`
+            if(r != 0 && c != 0){
+                cella.innerText = `${r} - ${c}`
+            }
             riga.append(cella)                              // Aggiungo la cella alla riga
 
         }
