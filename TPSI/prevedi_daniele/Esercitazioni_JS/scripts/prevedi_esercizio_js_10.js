854c4c3be62238bdcbc04c5c47b9781775bfbca5
Prevedi-Daniele <danieleprevedi@gmail.com>
2025-12-04 13:45:39 +0100

    (Prevedi) Consegna Esercitazione 10 JS

---
+++ TPSI/prevedi_daniele/Esercitazioni_JS/scripts/prevedi_esercizio_js_10.js
@@ -0,0 +1,9 @@
+function forInRicreato(oggetto){
+    let chiaviOggetto = Object.keys(oggetto)
+    
+    for (let i = 0; i < chiaviOggetto.length; i++) {
+        nomeChiave = chiaviOggetto[i]
+        
+        // Codice da eseguire nel for qui
+    }
+}
