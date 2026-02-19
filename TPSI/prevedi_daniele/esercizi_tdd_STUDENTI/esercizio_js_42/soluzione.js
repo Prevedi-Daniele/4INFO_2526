0adf8a8026789ac64d15927555f6d46b7b131a4e
Prevedi-Daniele <danieleprevedi@gmail.com>
2026-02-19 16:41:41 +0100

    .

--- TPSI/prevedi_daniele/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
+++ TPSI/prevedi_daniele/esercizi_tdd_STUDENTI/esercizio_js_42/soluzione.js
@@ -1,7 +1,7 @@
 export const swap = (l, n, m) => {
-    let x = l[n]
+    let x = l.at(n)
     l.splice(n, 1) // Toglie l'elemento alla posizione n
-    l.splice(m >= 0 ? m : m+1, 0, l[n])
+    l.splice(m >= 0 ? m : m+1, 0, x)
 
     return l
 };
