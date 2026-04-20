afc907509608565739569f965b8db0d7bbc82067
Prevedi-Daniele <danieleprevedi@gmail.com>
2026-04-20 09:57:11 +0200

    .

---
+++ TPSI/prevedi_daniele/Esercitazioni_JS_Node/prevedi_esercizio_js_71.js
@@ -0,0 +1,20 @@
+class Poligono {
+    constructor(lunghezzaLato, numeroLati){
+        this.lunghezzaLato = lunghezzaLato
+        this.numeroLati = numeroLati
+    }
+
+    perimetro(){
+        let p = 0
+        return p
+    }
+
+    area(){
+        let a = 0
+        return a
+    }
+}
+
+class Triangolo {
+    
+}
